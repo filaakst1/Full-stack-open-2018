accce7a7c051d1c6202bbcdbd454e1fc0b2d0750
filaakst1 <tomi.j.laakso@gmail.com>
2018-09-01 21:32:32 +0300

    Working version of exercise 1.3
    Changed data to objects and implemented Teact components to match new
    input data

--- Osa_1/1.3/src/index.js
+++ Osa_1/1.3/src/index.js
@@ -9,7 +9,7 @@ const Otsikko = (props) => {
 const Osa = (props) => {
     return (
         <div>
-            <p>{props.osa} {props.teht}</p>
+            <p>{props.osa.nimi} {props.osa.tehtavia}</p>
         </div>
     )
 
@@ -17,9 +17,9 @@ const Osa = (props) => {
 const Sisalto = (props) => {
     return(
         <div>
-            <Osa osa={props.osa1} teht={props.teht1} />
-            <Osa osa={props.osa2} teht={props.teht2} />
-            <Osa osa={props.osa3} teht={props.teht3} />
+            <Osa osa={props.osa1} />
+            <Osa osa={props.osa2} />
+            <Osa osa={props.osa3} />
         </div>
     )
 }
@@ -29,20 +29,26 @@ const Yhteensa = (props) => {
     )
 }
 const App = () => {
-  const kurssi = 'Half Stack -sovelluskehitys'
-  const osa1 = 'Reactin perusteet'
-  const tehtavia1 = 10
-  const osa2 = 'Tiedonvälitys propseilla'
-  const tehtavia2 = 7
-  const osa3 = 'Komponenttien tila'
-  const tehtavia3 = 14
+    const kurssi = 'Half Stack -sovelluskehitys'
+    const osa1 = {
+      nimi: 'Reactin perusteet',
+      tehtavia: 10
+    }
+    const osa2 = {
+      nimi: 'Tiedonvälitys propseilla',
+      tehtavia: 7
+    }
+    const osa3 = {
+      nimi: 'Komponenttien tila',
+      tehtavia: 14
+    }
 
   return (
     <div>
         <div>
             <Otsikko kurssi={kurssi} />
-            <Sisalto osa1={osa1} teht1={tehtavia1}  osa2={osa2} teht2={tehtavia2} osa3={osa3} teht3={tehtavia3} />
-            <Yhteensa yhteensa={tehtavia1+tehtavia2+tehtavia3} />
+            <Sisalto osa1={osa1} osa2={osa2} osa3={osa3} />
+            <Yhteensa yhteensa={osa1.tehtavia+osa2.tehtavia+osa3.tehtavia} />
         </div>
     </div>
   )
