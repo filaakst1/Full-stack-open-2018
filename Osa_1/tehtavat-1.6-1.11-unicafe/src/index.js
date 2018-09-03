fbf8fc44f4fc291f8253f9cf3de5efbabeac9f26
Tomi Laakso <tomi.j.laakso@gmail.com>
2018-09-03 14:12:54 +0300

    final version of exercise 1.7
    - Added fields for average and percentage of positive feedback
    - if no feedback given, displays 0 values insted of NaN

--- Osa_1/tehtavat-1.6-1.11-unicafe/src/index.js
+++ Osa_1/tehtavat-1.6-1.11-unicafe/src/index.js
@@ -4,16 +4,27 @@ import ReactDOM from 'react-dom'
 const MyButton = ({ handleClick, text }) => (
     <button onClick={handleClick}>{text}</button>
 )
-const MyStatistics = ({ good, neutral,bad }) => (
-    <div>
-        <h1>statistiikka</h1>
-        <dl>
-            <dt>hyvä {good}</dt>
-            <dt>neuraali {neutral}</dt>
-            <dt>huono {bad}</dt>
-        </dl>
-    </div>
-)
+
+const MyStatistics = (props) => {
+    const { good, neutral, bad } = props
+    const sum = good+bad+neutral
+    const average = sum!==0? ((good - bad)/sum) : 0
+    const positive = sum!==0? ((good*100)/sum) : 0
+
+    return (
+        <div>
+            <h1>statistiikka</h1>
+            <dl>
+                <dt>hyvä {good}</dt>
+                <dt>neuraali {neutral}</dt>
+                <dt>huono {bad}</dt>
+                <dt>keskiarvo {average.toFixed(1)}</dt>
+                <dt>positiivisia {positive.toFixed(1)} %</dt>
+            </dl>
+        </div>
+    )
+  }
+  
 class App extends React.Component {
     constructor() {
       super()
