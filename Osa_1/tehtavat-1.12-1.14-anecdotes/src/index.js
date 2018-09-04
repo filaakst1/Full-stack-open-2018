beae0840e79441d18a7889254ae3c0189fd79fe8
Tomi Laakso <tomi.j.laakso@gmail.com>
2018-09-04 14:15:32 +0300

    Final version of exercise 1.13.
    Added voting button and state that contains points as array

--- Osa_1/tehtavat-1.12-1.14-anecdotes/src/index.js
+++ Osa_1/tehtavat-1.12-1.14-anecdotes/src/index.js
@@ -4,12 +4,26 @@ import ReactDOM from 'react-dom'
 const Button = ({ handleClick, text }) => (
     <button onClick={handleClick}>{text}</button>
 )
+const AnecdoteDisplay = ({anecdote, points}) => {
+  const voteString = points===1 ? 'vote' : 'votes'
+  return (
+    <div>
+      <p>{anecdote}<br />has {points} {voteString}</p>
+    </div>
+  )
+}
 
 class App extends React.Component {
   constructor(props) {
     super(props)
+    const points = []
+    let anec
+    for(anec in props.anecdotes) {
+      points[anec] = 0
+    }
     this.state = {
-      selected: 0
+      selected: 0,
+      points : points
     }
   }
   nextAnecdote = (anecdotes) => {
@@ -22,11 +36,23 @@ class App extends React.Component {
         selected: nextIndex
     })
   }
+  vote = () =>{
+    const currentPoints = this.state.points
+    const index = this.state.selected
+    const copyOfPoints = [...currentPoints]
+    copyOfPoints[index] += 1
+    return () =>  this.setState({
+        points: copyOfPoints
+    })
+  }
   render() {
     return (
       <div>
-        <p>{this.props.anecdotes[this.state.selected]}</p>
-        <Button handleClick={this.nextAnecdote(this.props.anecdotes)} text="next anecdote" />
+        <AnecdoteDisplay anecdote={this.props.anecdotes[this.state.selected]} points={this.state.points[this.state.selected]} />
+        <div>
+          <Button handleClick={this.vote()} text="vote" />
+          <Button handleClick={this.nextAnecdote(this.props.anecdotes)} text="next anecdote" />
+        </div>
       </div>
     )
   }
