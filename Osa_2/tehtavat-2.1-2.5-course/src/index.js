15975cf0f173dab722c9a5b1970c2396f114bf8e
Tomi Laakso <tomi.j.laakso@gmail.com>
2018-09-27 21:56:42 +0300

    Initial commit
    Initial commit

---
+++ Osa_2/tehtavat-2.1-2.5-course/src/index.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Otsikko = (props) => {
+    return (
+        <h1>{props.kurssi.nimi}</h1>
+    )
+}
+const Osa = (props) => {
+    return (
+        <div>
+            <p>{props.osa.nimi} {props.osa.tehtavia}</p>
+        </div>
+    )
+
+}
+const Sisalto = (props) => {
+    return(
+        <div>
+            <Osa osa={props.kurssi.osat[0]} />
+            <Osa osa={props.kurssi.osat[1]} />
+            <Osa osa={props.kurssi.osat[2]} />
+        </div>
+    )
+}
+const Yhteensa = (props) => {
+    return (
+        <p>yhteensä {props.kurssi.osat[0].tehtavia + props.kurssi.osat[1].tehtavia +props.kurssi.osat[2].tehtavia} tehtävää</p>
+    )
+}
+const Kurssi = ({ kurssi }) => (
+    <div>
+        <Otsikko kurssi={kurssi} />
+        <Sisalto kurssi={kurssi} />
+        <Yhteensa kurssi={kurssi} />
+    </div>
+)
+const App = () => {
+    const kurssi = {
+        nimi: 'Half Stack -sovelluskehitys',
+        osat: [
+          {
+              nimi: 'Reactin perusteet',
+              tehtavia: 10
+          },
+          {
+              nimi: 'Tiedonvälitys propseilla',
+              tehtavia: 7
+          },
+          {
+              nimi: 'Komponenttien tila',
+              tehtavia: 14
+          }
+        ]
+    }
+    return (
+        <div>
+            <Kurssi kurssi={kurssi} />
+        </div>
+    )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
